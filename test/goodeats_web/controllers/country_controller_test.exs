1ab22989c6fa4ae705534d9dc80ed0440c8781df
Nicole Cody <ncody@cars.com>
2019-10-09 16:09:39 -0500

    Update country controller tests to look for valid data instead of generic headers

--- test/goodeats_web/controllers/country_controller_test.exs
+++ test/goodeats_web/controllers/country_controller_test.exs
@@ -14,31 +14,31 @@ defmodule GoodeatsWeb.CountryControllerTest do
 
   describe "index" do
     test "lists all countries", %{conn: conn} do
-      conn = get conn, country_path(conn, :index)
-      assert html_response(conn, 200) =~ "Listing Countries"
+      conn = get(conn, country_path(conn, :index))
+      assert html_response(conn, 200) =~ "Countries"
     end
   end
 
   describe "new country" do
     test "renders form", %{conn: conn} do
-      conn = get conn, country_path(conn, :new)
+      conn = get(conn, country_path(conn, :new))
       assert html_response(conn, 200) =~ "New Country"
     end
   end
 
   describe "create country" do
     test "redirects to show when data is valid", %{conn: conn} do
-      conn = post conn, country_path(conn, :create), country: @create_attrs
+      conn = post(conn, country_path(conn, :create), country: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
       assert redirected_to(conn) == country_path(conn, :show, id)
 
-      conn = get conn, country_path(conn, :show, id)
-      assert html_response(conn, 200) =~ "Show Country"
+      conn = get(conn, country_path(conn, :show, id))
+      assert html_response(conn, 200) =~ @create_attrs.name
     end
 
     test "renders errors when data is invalid", %{conn: conn} do
-      conn = post conn, country_path(conn, :create), country: @invalid_attrs
+      conn = post(conn, country_path(conn, :create), country: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Country"
     end
   end
@@ -47,7 +47,7 @@ defmodule GoodeatsWeb.CountryControllerTest do
     setup [:create_country]
 
     test "renders form for editing chosen country", %{conn: conn, country: country} do
-      conn = get conn, country_path(conn, :edit, country)
+      conn = get(conn, country_path(conn, :edit, country))
       assert html_response(conn, 200) =~ "Edit Country"
     end
   end
@@ -56,15 +56,15 @@ defmodule GoodeatsWeb.CountryControllerTest do
     setup [:create_country]
 
     test "redirects when data is valid", %{conn: conn, country: country} do
-      conn = put conn, country_path(conn, :update, country), country: @update_attrs
+      conn = put(conn, country_path(conn, :update, country), country: @update_attrs)
       assert redirected_to(conn) == country_path(conn, :show, country)
 
-      conn = get conn, country_path(conn, :show, country)
+      conn = get(conn, country_path(conn, :show, country))
       assert html_response(conn, 200) =~ "some updated name"
     end
 
     test "renders errors when data is invalid", %{conn: conn, country: country} do
-      conn = put conn, country_path(conn, :update, country), country: @invalid_attrs
+      conn = put(conn, country_path(conn, :update, country), country: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Country"
     end
   end
@@ -73,11 +73,12 @@ defmodule GoodeatsWeb.CountryControllerTest do
     setup [:create_country]
 
     test "deletes chosen country", %{conn: conn, country: country} do
-      conn = delete conn, country_path(conn, :delete, country)
+      conn = delete(conn, country_path(conn, :delete, country))
       assert redirected_to(conn) == country_path(conn, :index)
-      assert_error_sent 404, fn ->
-        get conn, country_path(conn, :show, country)
-      end
+
+      assert_error_sent(404, fn ->
+        get(conn, country_path(conn, :show, country))
+      end)
     end
   end
 
