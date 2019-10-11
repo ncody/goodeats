74aac5a1eb85ec6dcae3505418a2c028ab27d7a1
Nicole Cody <ncody@cars.com>
2019-10-11 11:09:08 -0500

    Only show rating for restaurants that have been tried

--- lib/goodeats_web/views/restaurant_view.ex
+++ lib/goodeats_web/views/restaurant_view.ex
@@ -5,14 +5,20 @@ defmodule GoodeatsWeb.RestaurantView do
     ["no", "yes", "want to try"]
   end
 
-  def display_rating(rating) do
-    case rating do
-      0 -> raw('<p>your rating: not rated</p>')
-      1 -> raw('<p>your rating: &#9733; &#9734; &#9734; &#9734; &#9734;</p>')
-      2 -> raw('<p>your rating: &#9733; &#9733; &#9734; &#9734; &#9734;</p>')
-      3 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9734; &#9734;</p>')
-      4 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9733; &#9734;</p>')
-      5 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9733; &#9733;</p>')
+  def display_rating(restaurant) do
+    case restaurant.tried do
+      "yes" ->
+        case restaurant.rating do
+          0 -> raw('<p>your rating: not rated</p>')
+          1 -> raw('<p>your rating: &#9733; &#9734; &#9734; &#9734; &#9734;</p>')
+          2 -> raw('<p>your rating: &#9733; &#9733; &#9734; &#9734; &#9734;</p>')
+          3 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9734; &#9734;</p>')
+          4 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9733; &#9734;</p>')
+          5 -> raw('<p>your rating: &#9733; &#9733; &#9733; &#9733; &#9733;</p>')
+        end
+
+      _ ->
+        nil
     end
   end
 
