e224350a42d2e99720da1d0159e698485d88b86d
Nicole Cody <ncody@cars.com>
2019-10-11 09:33:25 -0500

    Add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,51 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Goodeats.Repo
+alias Goodeats.Blog
+alias Goodeats.Blog.{Country, City, Restaurant}
+
+us =
+  Repo.insert!(%Country{
+    name: "United States"
+  })
+
+spain =
+  Repo.insert!(%Country{
+    name: "Spain"
+  })
+
+chicago =
+  Repo.insert!(%City{
+    name: "Chicago",
+    country: us
+  })
+
+minneapolis =
+  Repo.insert!(%City{
+    name: "Minneapolis"
+  })
+
+Repo.insert!(%Restaurant{
+  name: "Cruz Blanca",
+  cuisine: "Mexican",
+  description: "Rick Bayless restaurant in Fulton Market",
+  tried: "yes",
+  rating: 4,
+  city: chicago
+})
+
+Repo.insert!(%Restaurant{
+  name: "Picolo Sogno",
+  cuisine: "Italian",
+  tried: "want to try",
+  city: chicago
+})
+
+Repo.insert!(%Restaurant{
+  name: "Portafino",
+  cuisine: "Italian",
+  tried: "no",
+  city: chicago
+})
