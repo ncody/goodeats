d2f6bcef9f8cf2e21761f80c30ddaa94c90b4702
Nicole Cody <ncody@cars.com>
2019-10-15 11:30:50 -0500

    Sort cities and countries alphabetically

--- lib/goodeats_web/views/country_view.ex
+++ lib/goodeats_web/views/country_view.ex
@@ -4,4 +4,8 @@ defmodule GoodeatsWeb.CountryView do
   def has_cities?(country) do
     length(country.cities) > 0
   end
+
+  def sort_alphabetically(places) do
+    Enum.sort_by(places, fn place -> place.name end)
+  end
 end
